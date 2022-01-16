b9730d7db77fb39162210f3071b3f4fc64ed39ba
pankornch <pankornch@users.noreply.github.com>
2022-01-16 08:47:19 +0000

    deploy: f27dffb75f82b819dba14da364f279e149b124ab

---
+++ _next/static/4714LUH6bTPXHzXN6PG_v/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1-418c7f5e678d0e6e.js","static/chunks/pages/index-199cddbf0723f506.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
